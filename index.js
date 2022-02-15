1bd4e772cb10576da027c93dd4617c6d86d47233
Blankspace <officialpiksl@gmail.com>
2022-02-15 16:54:36 +0530

    Update 1.0.1

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const axios = require('axios').default;
 
 async function resolveAddress(address) {
-    let name = await axios.get("https://api.wnsdomains.io/resolve/address=" + address);
+    let name = await axios.get("https://resolver.wnsdomains.io/resolve/address=" + address);
     return name.data.value;
 }
 
 async function resolveName(name) {
-    let address = await axios.get("https://api.wnsdomains.io/resolve/name=" + name);
+    let address = await axios.get("https://resolver.wnsdomains.io/resolve/name=" + name);
     return address.data.value;
 }
 
 async function resolveNameAll(names) {
     let allnames = names.join("&");
-    let address = await axios.get("https://api.wnsdomains.io/resolve/name=" + allnames);
+    let address = await axios.get("https://resolver.wnsdomains.io/resolve/name=" + allnames);
     return address.data.value;
 }
 
 async function resolveAddressAll(address) {
     let alladdresses = address.join("&");
-    let names = await axios.get("https://api.wnsdomains.io/resolve/address=" + alladdresses);
+    let names = await axios.get("https://resolver.wnsdomains.io/resolve/address=" + alladdresses);
     return names.data.value;
 }
 
